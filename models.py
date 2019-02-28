f2f86df7455883f288ce59fa4185457b7c3b713b
Unknown <khvilaboa@esei.uvigo.es>
2019-02-28 19:00:18 +0100

    Some models

--- models.py
+++ models.py
@@ -1,2 +1,17 @@
 
 # TODO: Define models.
+
+class Photo:
+    def __init__(self, tags: set, horizontal: bool):
+        self.tags = tags
+        self.horizontal = horizontal
+
+class Slide:
+    def __init__(self, photo1, photo2 = None):
+        self.photo1 = photo1
+        self.photo2 = photo2
+
+        if photo2 is None:
+            self.tags = self.photo1.tags.copy()
+        else:
+            self.tags = photo1.tags | photo2.tags
